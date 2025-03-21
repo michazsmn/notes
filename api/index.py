190548ff11e03247b376ed17f0fb5cac63ea6ac3
Michael <michael.zusmanovskiy@gmail.com>
2025-03-21 17:51:28 +0100

    update endpoints

--- api/index.py
+++ api/index.py
@@ -8,9 +8,25 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 
 @app.get("/notes/", response_model=list[Note])
-def get_all_notes(db : Session = Depends(get_db)):
+async def get_all_notes(db : Session = Depends(get_db)):
     return get_notes(db)
 
 @app.post("/notes/", response_model=Note)
-def post_notes(note : NoteIn, db : Session = Depends(get_db)):
+async def post_notes(note : NoteIn, db : Session = Depends(get_db)):
     return create_note(db, note)
+
+@app.put("/notes/{id}", response_model=Note)
+async def update_note(id : int, db : Session = Depends(get_db)):
+    db_update = toggle_status(db, id)
+    if db_update:
+        return db_update
+    else:
+        raise HTTPException(status_code=404, detail="invalid id")
+
+@app.delete("/notes/{id}", response_model=Note)
+async def delete_note(id : int, db : Session = Depends(get_db)):
+    deleted_note = del_note(db, id)
+    if deleted_note:
+        return deleted_note
+    else:
+        raise HTTPException(status_code=404, detail="invalid id")
--- api/services.py
+++ api/services.py
@@ -12,3 +12,25 @@ def create_note(db : Session, data : NoteIn):
 
 def get_notes(db : Session):
     return db.query(notes).all()
+
+def get_note_by_id(db : Session, id : int):
+    return db.query(notes).filter(notes.id == id).first()
+
+def toggle_status(db : Session, id : int):
+    note_query = db.query(notes).filter(notes.id == id).first()
+    if note_query:
+        #note_query_dict = note_query.model_dump().items
+        setattr(note_query, "completed", not getattr(note_query, "completed"))
+        db.commit()
+        db.refresh(note_query)
+    return note_query
+
+def del_note(db : Session, id : int):
+    note_query = db.query(notes).filter(notes.id == id).first()
+    if note_query:
+        db.delete(note_query)
+        db.commit()
+    return note_query
+
+        
+        
